341f529c8dff5833ee41d921c9ba2c5faa8a0b6e
MoeQamhieh <45400921+MoeQamhieh@users.noreply.github.com>
2022-03-30 01:22:06 +0100

    Steak spawning fix

--- COMP0113-CW2/Assets/Scripts/ServingTray.cs
+++ COMP0113-CW2/Assets/Scripts/ServingTray.cs
@@ -14,12 +14,12 @@ public class ServingTray : MonoBehaviour
     void OnCollisionEnter(Collision other)
     {
         
-        if (other.gameObject.name == "Plate" || other.gameObject.name == "SmallPlate")
+        if ((other.gameObject.name == "Plate" || other.gameObject.name == "SmallPlate") & other.gameObject.GetComponent<Spawned>().owner)
         {
             Transform[] children = other.gameObject.GetComponentsInChildren<Transform>();
             switch(parentScript.currentPanel.name)
             {
-                /*case "Sandwich Panel":
+                case "Sandwich Panel":
                     List<Transform> loaf = GetChildrenWithName(other.gameObject.transform, "CutLoaf");
                     List<Transform> cheese = GetChildrenWithName(other.gameObject.transform, "CutCutCheese");
                     List<Transform> tomato = GetChildrenWithName(other.gameObject.transform, "CutTomato");
@@ -39,7 +39,6 @@ public class ServingTray : MonoBehaviour
                         parentScript.FailOrder();
                     }
                     break;
-                 */
                 case "Steak Panel":
                     List<Transform> steak = GetChildrenWithName(other.gameObject.transform, "CookedMeat");
                     if (steak.Count == 1 & children.Length == 2)
@@ -98,7 +97,7 @@ public class ServingTray : MonoBehaviour
             t -= 1f*Time.deltaTime;
         }
 
-        Destroy(ToMove);
+        ToMove.GetComponent<Spawned>().destroy = true;
         yield break;
     }
 
